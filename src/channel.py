ef93802d74850246305d9990de305652bb6de7e0
German <germanstarinenko@gmail.com>
2023-04-06 09:51:06 +0300

    a1

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,13 @@
-
+import json
 
 class Channel:
     """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        with open("text.json","r") as file:
+            for i in file:
+                print(json.dumps(i.strip('\n,",",{},[]'), indent=2, ensure_ascii=False))
