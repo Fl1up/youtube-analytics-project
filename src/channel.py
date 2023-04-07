60c3bbef51cdf45eba193f8e1b2fffeb0c5b377e
German <germanstarinenko@gmail.com>
2023-04-07 15:44:15 +0300

    good

--- src/channel.py
+++ src/channel.py
@@ -1,13 +1,22 @@
-import json
+import json, os
+from googleapiclient.discovery import build
+
 
 class Channel:
     """Класс для ютуб-канала"""
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        with open("text.json","r") as file:
-            for i in file:
-                print(json.dumps(i.strip('\n,",",{},[]'), indent=2, ensure_ascii=False))
+        printj(self.channel)
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
